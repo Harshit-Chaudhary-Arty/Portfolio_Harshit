44a3a35bbc7dee0652c9a366964e016e025a9ce6
Harshit Chaudhary <harshit.chaudhary.arty@gmail.com>
2024-02-13 17:51:04 +0530

    page1

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ tl.from("#loader h1, #loader h2",{
     duration:1.4,
     scale: 1.1,
     filter: "blur(9px)",
-
-
 })
 
 tl.to("#loader h1, #loader h2",{
@@ -17,5 +15,21 @@ tl.to("#loader h1, #loader h2",{
     duration:0.3,
     filter: "blur(7px)",
     delay:0.7,
-    
+    display:"none",
+})
+
+tl.to("#loader",{
+    y: 100,
+    opacity:0,
+    // blur:("60px"),
+    duration:0.3,
+    filter: "blur(7px)",
+    delay:0,
+})
+
+tl.from("#page1 #nav #nleft, #page1 #nav #nright",{
+    opacity:0,
+    y:-50,
+    stagger,
+    duration:1,
 })
